094755c67a71168d07c4f69188072757229d85c7
Siddi-Venkatesh <siddivenkatesh111@gmail.com>
2026-06-20 21:30:42 +0530

    style: resize chat assistant to be even wider and shorter

--- src/components/ChatAssistant.tsx
+++ src/components/ChatAssistant.tsx
@@ -50,8 +50,8 @@ export const ChatAssistant: React.FC = () => {
       
       {isOpen && (
         <Card style={{ 
-          width: '420px', 
-          height: '450px', 
+          width: '500px', 
+          height: '400px', 
           marginBottom: '1rem', 
           display: 'flex', 
           flexDirection: 'column',
